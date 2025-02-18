9deda4a46937437d966045d922fadec2734a0c2a
Vsevolod Luzhaev <luzaevvsevolod@gmail.com>
2025-02-18 08:35:19 +0300

    Add db context

---
+++ TgBotAgent/TgBotAgent/DB/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TgBotAgent.DB
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Setting> Settings { get; set; }
+        public DbSet<UserLink> UserLinks { get; set; }
+        public DbSet<MessageRecord> Messages { get; set; }
+        public DbSet<BlacklistWord> BlacklistWords { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // Подключение к PostgreSQL
+            optionsBuilder.UseNpgsql("Host=localhost;Database=tgagent;Username=postgres;Password=12345Ob@");
+        }
+    }
+}
